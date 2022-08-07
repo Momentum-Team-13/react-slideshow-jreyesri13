6335bfc85e889ace0e415d796169910df95673f2
Jose Reyes <jreyesri@cbu.edu>
2022-08-06 22:40:20 -0400

    Try and try again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css'
-import Slider from './Slideshow.js';
+
 // import * as dataSlider from './film-data.json'
 
 function App() {
   return (
-    <>
-      <Slider />
-    </>
-    // <div className="slides-app">
-    //   <h1>Your React App</h1>
-    // </div>
+    <div>
+      <h1>Your React App</h1>
+    </div>
   )
 }
 
