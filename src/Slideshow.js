ed82dfb08eb7a5416460c87161b6fa10846119f2
Jose Reyes <jreyesri@cbu.edu>
2022-08-07 01:36:23 -0400

    Basic function is working

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,12 +1,80 @@
 import React, { useState } from 'react';
-import data from './film-data.json';
-
+import dataSlider from './film-data.json';
 
 export default function Slider() {
 
+    // const [slide, setSlide] = useState([])
+
+    const [count, setCount] = useState(0)
+
+
+    const sliderLength = dataSlider.length - 1;
+
+    const handleNext = () => {
+        if (count < sliderLength) {
+            setCount(count + 1)
+        }
+
+        // if (count === sliderLength) {
+        //     setCount(0)
+        // }
+    }
+
+    const handleBack = () => {
+
+        if (count > 0) {
+            setCount(count - 1)
+        }
+
+        // if (count === 0) {
+        //     setCount(sliderLength)
+        // }
+    }
+
+    const handleReset = () => {
+        setCount(0)
+    }
+
+
+
     return (
         <>
-            <h1>Hello World</h1>
+            {/* <p>Total Length: {sliderLength}</p> */}
+
+            <p>Count: {count}</p>
+
+            <button onClick={() => handleNext()}>
+                Next
+            </button>
+
+            <br />
+
+            <button onClick={() => handleBack()}>
+                Back
+            </button>
+
+            <br />
+
+            <button onClick={() => handleReset()}>
+                Start Over
+            </button>
+
+            <p>{dataSlider[count].title}</p>
+
+            <p>{dataSlider[count].original_title}</p>
+
+            <p>{dataSlider[count].release_date}</p>
+
+            <p>{dataSlider[count].description}</p>
+
+            <img src={dataSlider[count].image} alt={dataSlider[count].title} width="300"></img>
+
+
+
+
+            {/* {dataSlider.map((itemz, index) => (
+                <p key={index}>{itemz.title}</p>
+            ))} */}
         </>
     )
 }
